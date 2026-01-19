eef8dceb7bccac0b1042d886d8b764a01560d171
transcrime <xta12wq7@portforwarding.anonaddy.com>
2026-01-19 22:51:33 +0000

    Add files via upload

---
+++ config-setup/fish/config.fish
@@ -0,0 +1,12 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/nora/.lmstudio/bin
+# End of LM Studio CLI section
+
